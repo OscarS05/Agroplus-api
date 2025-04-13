3d1a99acd4c56decf426b0081f940b877293b0d2
OscarS05 <santiagom0509@hotmail.com>
2025-04-12 22:29:47 -0500

    cambio pequeño para intentar sincronizar

--- src/modules/user/user.repository.js
+++ src/modules/user/user.repository.js
@@ -4,6 +4,11 @@ const findOne = async (userId) => {
   return await sequelize.models.User.findOne({ where: { id: userId } });
 }
 
+const create = async (userId) => {
+  // return await sequelize.models.User.findOne({ where: { id: userId } });
+}
+
 module.exports = {
   findOne,
+  create,
 }
